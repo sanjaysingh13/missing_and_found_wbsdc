719c9658bc35270760f01f3c0d7a0621f4a87144
Sanjay <sanjaysingh13@gmail.com>
2023-01-13 23:11:55 +0530

    AWS

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,11 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["wbkhoyapaya.com", ".ccsneo4jdigitalocean.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS",
+                         default=["wbkhoyapaya.com",
+                                  "52.66.130.155",
+                                  ".ccsneo4jdigitalocean.com"])
+
 
 # DATABASES
 # ------------------------------------------------------------------------------
