2101daf9c1431538ae28bb57e48fecb69a8f0fa1
Sanjay <sanjaysingh13@gmail.com>
2023-02-14 11:58:17 +0530

    favicon issue

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,21 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 
+
+def home(request):
+    if request.get_host() == "wbkhoyapaya.com":
+        return redirect("http://www.wbkhoyapaya.com/")
+    else:
+        return TemplateView.as_view(template_name="pages/home.html")(request)
+
+
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("", home, name="home"),
     path(
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
     ),
