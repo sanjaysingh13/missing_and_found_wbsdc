ed1978f39e4a6eb605148f6d38e3160c45818a2a
Sanjay <sanjaysingh13@gmail.com>
2023-02-17 20:37:11 +0530

    deletion of emailaddress

--- missing_persons_match_unidentified_dead_bodies/users/signals.py
+++ missing_persons_match_unidentified_dead_bodies/users/signals.py
@@ -1,6 +1,6 @@
 from allauth.account.models import EmailAddress
 from django.contrib.auth.models import Group
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
 from .models import User
@@ -18,5 +18,14 @@ def verify_user(sender, instance, **kwargs):
         email_address = EmailAddress.objects.filter(user=instance)
         email_address[0].verified = True
         email_address[0].save()
+    except Exception as e:
+        print(str(e))
+
+
+@receiver(pre_delete, sender=User)
+def delete_user_email_address(sender, instance, **kwargs):
+    try:
+        email_address = EmailAddress.objects.filter(user=instance)
+        email_address[0].delete()
     except Exception as e:
         print(str(e))
